ab8f3c9d8e9d04d1ae8ebd00ba174ae66d63882f
tdingsun <tiger@Tigers-MacBook-Pro.local>
2017-08-09 12:13:03 -0700

    More html

--- johngrue/script.js
+++ johngrue/script.js
@@ -1,30 +1,24 @@
-var wordArray = [];
+//var wordArray = [];
 var nouns = ["people", "rain", "a drizzle", "a hurricane"];
-var nounOne = "people";
-var nounTwo = "rain";
-var nounThree = "a drizzle";
-var nounFour = "a hurricane";
-var leading = 55;
-var leftMargin = 100;
-var topMargin = 100;
+// var leading = 55;
+// var leftMargin = 100;
+// var topMargin = 100;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	wordArray = RiTa.alliterations("cat", 10);
-	print("works");
-	print(wordArray);
+	//wordArray = RiTa.alliterations("cat", 10);
 }
 
 function draw() {
 	background(255,255,255);
-	textSize(50);
-	textAlign(CENTER);
+	//textSize(50);
+	//textAlign(CENTER);
 	// for(var i = 0; i < 10; i++){
 
 	// 	text(wordArray[i], leftMargin, topMargin+leading*i);
 	// }
 
-	if(frameCount % 60 == 0){
+	if(frameCount % 90 == 0){
 		// var newArray = RiTa.alliterations("cat", 1);
 		// wordArray[int(random(10))] = newArray[0];
 
@@ -51,16 +45,39 @@ function draw() {
 		}
 
 	}
-	if (windowWidth > 550){
-		text("If " + nouns[0] + " were " + nouns[1] + ",\nI was " + nouns[2] + " and she was " + nouns[3] + ".", windowWidth/2, windowHeight/2 - 100);
-		text("- John Green", 3*windowWidth/4, windowHeight/2 + 100);
-	} else {
-		textSize(30);
-		textAlign(LEFT);
-		text("If " + nouns[0] + "\nwere " + nouns[1] + ",\nI was " + nouns[2] + "\nand she was\n" + nouns[3] + ".", windowWidth/6, windowHeight/2 - 100);
-		text("- John Green", windowWidth/3, 3*windowHeight/4);
-	}
-	
+
+	// if (windowWidth > 550){
+	// 	text("If " + nouns[0] + " were " + nouns[1] + ",\nI was " + nouns[2] + " and she was " + nouns[3] + ".", windowWidth/2, windowHeight/2 - 100);
+	// 	text("- John Green", 3*windowWidth/4, windowHeight/2 + 100);
+	// } else {
+	// 	textSize(30);
+	// 	textAlign(LEFT);
+	// 	text("If " + nouns[0] + "\nwere " + nouns[1] + ",\nI was " + nouns[2] + "\nand she was\n" + nouns[3] + ".", windowWidth/6, windowHeight/2 - 100);
+	// 	text("- John Green", windowWidth/3, 3*windowHeight/4);
+	// }
+
+	document.getElementById("one").innerHTML = nouns[0];
+	document.getElementById("two").innerHTML = nouns[1];
+	document.getElementById("three").innerHTML = nouns[2];
+	document.getElementById("four").innerHTML = nouns[3];
+
+	document.getElementById("one").onmousedown = function() {
+		nouns[0] = "people"
+	};
+
+	document.getElementById("two").onmousedown = function() {
+		nouns[1] = "rain"
+	};
+
+	document.getElementById("three").onmousedown = function() {
+		nouns[2] = "a drizzle"
+	};
+	document.getElementById("four").onmousedown = function() {
+		nouns[3] = "a hurricane"
+	};
+
 }
+
+
 
 
