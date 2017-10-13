93eb8b9b3339a1e0e9fe1b4ede447f2fe5d40f91
tdingsun <tiger@Tigers-MBP.fios-router.home>
2017-10-13 03:22:06 -0400

    Update

--- otm/script.js
+++ otm/script.js
@@ -1,20 +1,77 @@
+var w;
 $(document).ready(function(){
 	$("#about").hide();
-	$("#about-button").click(function() {
-		$("#about").slideToggle();
+	$(".episode-container").hide();
 
-		if ( $("#about-button").text() == "[ABOUT]" ){
-			// $("#about-button").text("[HIDE]");
-			// $("#about-button").css("top", "42px");
-			// $("#about-button").css("right", "40px");
-			// $("#about-button").css("font-size", "1em");
-		} else {
-			$("#about-button").css("font-family", "sans-serif");
-			$("#about-button").css("color", "#F05324");
-			$("#about-button").text("[?]");
-			$("#about-button").css("top", "30px");
-			$("#about-button").css("right", "20px");
-			$("#about-button").css("font-size", "1.4em");
-		}
+	//about toggle
+	$("#about-container").hover(function() {
+		$("#hr-about").toggleClass('hr-long');
 	});
+	$("#about-container").click(function() {
+		$("#about").slideToggle();
+	});
+
+	//episode toggle -- gotta change from slidetoggle
+	//to toggleclass
+	$(".episode-button").hover(function() {
+		var hr = jQuery(this).find(".hr-long");
+		$(hr).toggleClass("hr-white");
+	});
+
+	$(".episode").click(function() {
+		var ec = jQuery(this).find(".episode-container");
+		var eb = jQuery(this).find(".episode-button");
+		
+		//open or close current module
+		var st = $("body").scrollTop();
+		$(ec).slideToggle(250);
+
+		
+		// var sibs = $(this).siblings(".episode");
+		// var other_ec = jQuery(sibs).find(".episode-container");
+		// $(other_ec).slideUp();
+		$(eb).toggleClass("episode-button-open");
+	});
+
+
+	//Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.nav').outerHeight();
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('.nav').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('.nav').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+
 });
+
