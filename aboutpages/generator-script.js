d529e667abc018f6fd5bbd85d873601ff2e0d501
tdingsun <tiger@Tigers-MBP.fios-router.home>
2017-10-15 14:57:09 -0400

    Better

--- aboutpages/generator-script.js
+++ aboutpages/generator-script.js
@@ -9,143 +9,74 @@ function preload() {
 }
 
 function setup() {
+	lyrics = data.join(" ");
 
-		let rm_2 = new RiMarkov(2);
-		let rm_3 = new RiMarkov(3);
-		let rm_4 = new RiMarkov(4);
-		let rm_5 = new RiMarkov(5);
-		let rm_6 = new RiMarkov(6);
+	for(int i = 2; i < 6; i++){
+		let rm = new RiMarkov(i);
+		rm.loadText(lyrics);
+		let sentences = rm.generateSentences(5);
 
-		lyrics = data.join(" ");
-		rm_2.loadText(lyrics);
-		rm_3.loadText(lyrics);
-		rm_4.loadText(lyrics);
-		rm_5.loadText(lyrics);
-		rm_6.loadText(lyrics);
-
-
-		let sentences_2 = rm_2.generateSentences(10);
-		let sentences_3 = rm_3.generateSentences(10);
-		let sentences_4 = rm_4.generateSentences(10);
-		let sentences_5 = rm_5.generateSentences(10);
-		let sentences_6 = rm_6.generateSentences(10);
-
-		let p_2 = document.createElement("p");
-		let p_3 = document.createElement("p");
-		let p_4 = document.createElement("p");
-		let p_5 = document.createElement("p");
-		let p_6 = document.createElement("p");
-
-		for (var i = 0; i < sentences_6.length; i++){
-			p_6.innerHTML += sentences_6[i];
-		}
-		for (var i = 0; i < sentences_5.length; i++){
-			p_5.innerHTML += sentences_5[i];
-		}
-		for (var i = 0; i < sentences_4.length; i++){
-			p_4.innerHTML += sentences_4[i];
-		}
-		for (var i = 0; i < sentences_3.length; i++){
-			p_3.innerHTML += sentences_3[i];
-		}
-		for (var i = 0; i < sentences_2.length; i++){
-			p_2.innerHTML += sentences_2[i];
+		let p = document.document.createElement("p");
+		for (var j = 0; j < sentences[j].length; j++){
+			p.innerHTML += sentences[j];
+			p.innerHTML += " ";
 		}
 
-		document.getElementById("container").appendChild(p_6);
-		document.getElementById("container").appendChild(p_5);
-		document.getElementById("container").appendChild(p_4);
-		document.getElementById("container").appendChild(p_3);
-		document.getElementById("container").appendChild(p_2);
-}
+		document.getElementById("container").appendChild(p);
 
-//your basic insertion_sort algorithm
-//o(n squared)
-function insertion_sort(array){
-	let n = array.length;
-	let temp;
-
-	for (let i = 1; i <= n-1; i++){
-		for(let j = i; j >= 1; j--){
-			if (array[j][1] > array[j-1][1]){
-				temp = array[j];
-				array[j] = array[j-1];
-				array[j-1] = temp;
-			} else {
-				break;
-			}
-		}
 	}
-}
+		// let rm_2 = new RiMarkov(2);
+		// let rm_3 = new RiMarkov(3);
+		// let rm_4 = new RiMarkov(4);
+		// let rm_5 = new RiMarkov(5);
+		// let rm_6 = new RiMarkov(6);
 
-//o(n squared)
-function selection_sort(a){
-	let n = a.length;
-	let temp;
-	let max;
-	let max_index;
-	for (let i = 0; i <= n-2; i++){
-		//find max
-		max = a[i][1];
-		max_index = i;
-		for (let j = i; j <= n-1; j++){
-			if (a[j][1] > max){
-				max = a[j][1];
-				max_index = j;
-			}
-		}
-		//swap
-		temp = a[max_index];
-		a[max_index] = a[i];
-		a[i] = temp;
-	}
-}
+		// lyrics = data.join(" ");
+		// rm_2.loadText(lyrics);
+		// rm_3.loadText(lyrics);
+		// rm_4.loadText(lyrics);
+		// rm_5.loadText(lyrics);
+		// rm_6.loadText(lyrics);
 
-function kwic(index){
 
-	//when you click on a word, it turns red, and then the word you previously clicked returns to grey
-	var element = document.getElementById("entry-" + index);
-	
-	$('html, body').animate({
-    scrollTop: $(element).offset().top - 150
-	}, 1000);
+		// let sentences_2 = rm_2.generateSentences(10);
+		// let sentences_3 = rm_3.generateSentences(10);
+		// let sentences_4 = rm_4.generateSentences(10);
+		// let sentences_5 = rm_5.generateSentences(10);
+		// let sentences_6 = rm_6.generateSentences(10);
 
-	element.style.color = "#083";
-	if (prevElement != null && prevElement != element){
-		prevElement.style.color = "#555";
-	}
-	prevElement = element;
+		// let p_2 = document.createElement("p");
+		// let p_3 = document.createElement("p");
+		// let p_4 = document.createElement("p");
+		// let p_5 = document.createElement("p");
+		// let p_6 = document.createElement("p");
 
-	let args = {
-		wordCount: 5,
-		ignoreCase: true,
-		ignorePunctuation: true
-	}
+		// for (var i = 0; i < sentences_6.length; i++){
+		// 	p_6.innerHTML += sentences_6[i];
+		// 	p_6.innerHTML += " ";
+		// }
+		// for (var i = 0; i < sentences_5.length; i++){
+		// 	p_5.innerHTML += sentences_5[i];
+		// 	p_5.innerHTML += " ";
+		// }
+		// for (var i = 0; i < sentences_4.length; i++){
+		// 	p_4.innerHTML += sentences_4[i];
+		// 	p_4.innerHTML += " ";
+		// }
+		// for (var i = 0; i < sentences_3.length; i++){
+		// 	p_3.innerHTML += sentences_3[i];
+		// 	p_3.innerHTML += " ";
+		// }
+		// for (var i = 0; i < sentences_2.length; i++){
+		// 	p_2.innerHTML += sentences_2[i];
+		// 	p_2.innerHTML += " ";
+		// }
 
-	let word = c_array[index][0]
-
-	var kw = RiTa.kwic(lyrics, word, args);
-	var kw_frame = document.getElementById("kwic-container");
-	var kw_frame_position = (element.getBoundingClientRect().top + window.pageYOffset);
-	kw_frame.style.margin = kw_frame_position + 'px 0 0 0';
-
-	kw_frame.innerHTML = ''; //resets the text every time a word is clicked
-
-	for (let i = 0; i < kw.length; i++){ //for every string in the kwic
-		var parts = kw[i].toLowerCase().split(word); //splits the string based the word
-
-		kw_frame.innerHTML += "... "; //start every line with "..."
-
-		for (let j = 0; j < parts.length; j++){ //for every part of the string that we split before
-			
-			if (j == (parts.length - 1)){ //end the line with ... and a line break
-				kw_frame.innerHTML += parts[j] + " ... <br>";
-			} else { // put all the parts in the innerHTML with the word (in red) between the parts.
-				kw_frame.innerHTML += parts[j] + "<span style='color: #083; border: 1px solid #083; padding: 0 5px 0 5px; margin: 5px  0 5px 0;'>" + word + "</span>";
-			}
-		}
-	}
-	
+		// document.getElementById("container").appendChild(p_6);
+		// document.getElementById("container").appendChild(p_5);
+		// document.getElementById("container").appendChild(p_4);
+		// document.getElementById("container").appendChild(p_3);
+		// document.getElementById("container").appendChild(p_2);
 }
 
 
