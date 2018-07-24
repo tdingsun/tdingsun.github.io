3c38b5cbd6d598c5996fa675c07a99fbf7eb91a3
tdingsun <tiger.dingsun@gmail.com>
2018-07-24 09:59:11 -0400

    Add files via upload

---
+++ cards/script.js
@@ -0,0 +1,103 @@
+//var text_array = RiTa.splitSentences("One, two. Baby really hurt me, crying in the taxi. He don't wanna know me says he made the big mistake of dancing in my storm. Says it was poison. So I guess I'll go home into the arms of the girl that I love. The only love I haven't screwed up. She's so hard to please but she's a forest fire. I do my best to meet her demands, play at romance, We slow dance in the living room, but all that a stranger would see. Is one girl swaying alone, stroking her cheek. They say, \"You\'re a little much for me, you\'re a liability. You\'re a little much for me.\" So they pull back, make other plans. I understand, I\'m a liability. Get you wild, make you leave. I\'m a little much for e-a-na-na-na, everyone. The truth is I am a toy that people enjoy \'til all of the tricks don't work anymore. And then they are bored of me. I know that it\'s exciting running through the night, But every perfect summer's eating me alive until you\'re gone Better on my own. They say, \"You're a little much for me, you're a liability. You're a little much for me\" So they pull back, make other plans, I understand, I'm a liability. Get you wild, make you leave, I'm a little much for e-a-na-na-na, everyone. They're gonna watch me disappear into the sun. You're all gonna watch me disappear into the sun.");
+var regex = /[.;:]/;
+var text_array = RiTa.splitSentences("For a long time I used to go to bed early. Sometimes, when I had put out my candle, my eyes would close so quickly that I had not even time to say \"I\'m going to sleep.\" And half an hour later the thought that it was time to go to sleep would awaken me; I would try to put away the book which, I imagined, was still in my hands, and to blow out the light; I had been thinking all the time, while I was asleep, of what I had just been reading, but my thoughts had run into a channel of their own, until I myself seemed actually to have become the subject of my book: a church, a quartet, the rivalry between François I and Charles V. This impression would persist for some moments after I was awake; it did not disturb my mind, but it lay like scales upon my eyes and prevented them from registering the fact that the candle was no longer burning. Then it would begin to seem unintelligible, as the thoughts of a former existence must be to a reincarnate spirit; the subject of my book would separate itself from me, leaving me free to choose whether I would form part of it or no; and at the same time my sight would return and I would be astonished to find myself in a state of darkness, pleasant and restful enough for the eyes, and even more, perhaps, for my mind, to which it appeared incomprehensible, without a cause, a matter dark indeed. I would ask myself what o\'clock it could be; I could hear the whistling of trains, which, now nearer and now farther off, punctuating the distance like the note of a bird in a forest, shewed me in perspective the deserted countryside through which a traveller would be hurrying towards the nearest station: the path that he followed being fixed for ever in his memory by the general excitement due to being in a strange place, to doing unusual things, to the last words of conversation, to farewells exchanged beneath an unfamiliar lamp which echoed still in his ears amid the silence of the night; and to the delightful prospect of being once again at home. I would lay my cheeks gently against the comfortable cheeks of my pillow, as plump and blooming as the cheeks of babyhood. Or I would strike a match to look at my watch. Nearly midnight. The hour when an invalid, who has been obliged to start on a journey and to sleep in a strange hotel, awakens in a moment of illness and sees with glad relief a streak of daylight shewing under his bedroom door. Oh, joy of joys! it is morning. The servants will be about in a minute: he can ring, and some one will come to look after him. The thought of being made comfortable gives him strength to endure his pain. He is certain he heard footsteps: they come nearer, and then die away. The ray of light beneath his door is extinguished. It is midnight; some one has turned out the gas; the last servant has gone to bed, and he must lie all night in agony with no one to bring him any help. I would fall asleep, and often I would be awake again for short snatches only, just long enough to hear the regular creaking of the wainscot, or to open my eyes to settle the shifting kaleidoscope of the darkness, to savour, in an instantaneous flash of perception, the sleep which lay heavy upon the furniture, the room, the whole surroundings of which I formed but an insignificant part and whose unconsciousness I should very soon return to share. Or, perhaps, while I was asleep I had returned without the least effort to an earlier stage in my life, now for ever outgrown; and had come under the thrall of one of my childish terrors, such as that old terror of my great-uncle\'s pulling my curls, which was effectually dispelled on the day--the dawn of a new era to me--on which they were finally cropped from my head. I had forgotten that event during my sleep; I remembered it again immediately I had succeeded in making myself wake up to escape my great-uncle\'s fingers; still, as a measure of precaution, I would bury the whole of my head in the pillow before returning to the world of dreams. Sometimes, too, just as Eve was created from a rib of Adam, so a woman would come into existence while I was sleeping, conceived from some strain in the position of my limbs. Formed by the appetite that I was on the point of gratifying, she it was, I imagined, who offered me that gratification. My body, conscious that its own warmth was permeating hers, would strive to become one with her, and I would awake. The rest of humanity seemed very remote in comparison with this woman whose company I had left but a moment ago: my cheek was still warm with her kiss, my body bent beneath the weight of hers. If, as would sometimes happen, she had the appearance of some woman whom I had known in waking hours, I would abandon myself altogether to the sole quest of her, like people who set out on a journey to see with their own eyes some city that they have always longed to visit, and imagine that they can taste in reality what has charmed their fancy. And then, gradually, the memory of her would dissolve and vanish, until I had forgotten the maiden of my dream. When a man is asleep, he has in a circle round him the chain of the hours, the sequence of the years, the order of the heavenly host. Instinctively, when he awakes, he looks to these, and in an instant reads off his own position on the earth\'s surface and the amount of time that has elapsed during his slumbers; but this ordered procession is apt to grow confused, and to break its ranks. Suppose that, towards morning, after a night of insomnia, sleep descends upon him while he is reading, in quite a different position from that in which he normally goes to sleep, he has only to lift his arm to arrest the sun and turn it back in its course, and, at the moment of waking, he will have no idea of the time, but will conclude that he has just gone to bed. Or suppose that he gets drowsy in some even more abnormal position; sitting in an armchair, say, after dinner: then the world will fall topsy-turvy from its orbit, the magic chair will carry him at full speed through time and space, and when he opens his eyes again he will imagine that he went to sleep months earlier and in some far distant country. But for me it was enough if, in my own bed, my sleep was so heavy as completely to relax my consciousness; for then I lost all sense of the place in which I had gone to sleep, and when I awoke at midnight, not knowing where I was, I could not be sure at first who I was; I had only the most rudimentary sense of existence, such as may lurk and flicker in the depths of an animal\'s consciousness; I was more destitute of human qualities than the cave-dweller; but then the memory, not yet of the place in which I was, but of various other places where I had lived, and might now very possibly be, would come like a rope let down from heaven to draw me up out of the abyss of not-being, from which I could never have escaped by myself: in a flash I would traverse and surmount centuries of civilisation, and out of a half-visualised succession of oil-lamps, followed by shirts with turned-down collars, would put together by degrees the component parts of my ego. Perhaps the immobility of the things that surround us is forced upon them by our conviction that they are themselves, and not anything else, and by the immobility of our conceptions of them. For it always happened that when I awoke like this, and my mind struggled in an unsuccessful attempt to discover where I was, everything would be moving round me through the darkness: things, places, years. My body, still too heavy with sleep to move, would make an effort to construe the form which its tiredness took as an orientation of its various members, so as to induce from that where the wall lay and the furniture stood, to piece together and to give a name to the house in which it must be living. Its memory, the composite memory of its ribs, knees, and shoulder-blades offered it a whole series of rooms in which it had at one time or another slept; while the unseen walls kept changing, adapting themselves to the shape of each successive room that it remembered, whirling madly through the darkness. And even before my brain, lingering in consideration of when things had happened and of what they had looked like, had collected sufficient impressions to enable it to identify the room, it, my body, would recall from each room in succession what the bed was like, where the doors were, how daylight came in at the windows, whether there was a passage outside, what I had had in my mind when I went to sleep, and had found there when I awoke. The stiffened side underneath my body would, for instance, in trying to fix its position, imagine itself to be lying, face to the wall, in a big bed with a canopy; and at once I would say to myself, \"Why, I must have gone to sleep after all, and Mamma never came to say good night!\" for I was in the country with my grandfather, who died years ago; and my body, the side upon which I was lying, loyally preserving from the past an impression which my mind should never have forgotten, brought back before my eyes the glimmering flame of the night-light in its bowl of Bohemian glass, shaped like an urn and hung by chains from the ceiling, and the chimney-piece of Siena marble in my bedroom at Combray, in my great-aunt\'s house, in those far distant days which, at the moment of waking, seemed present without being clearly penned, but would become plainer in a little while when I was properly awake. Then would come up the memory of a fresh position; the wall slid away in another direction; I was in my room in Mme. de Saint-Loup\'s house in the country; good heavens, it must be ten o\'clock, they will have finished dinner! I must have overslept myself, in the little nap which I always take when I come in from my walk with Mme. de Saint-Loup, before dressing for the evening. For many years have now elapsed since the Combray days, when, coming in from the longest and latest walks, I would still be in time to see the reflection of the sunset glowing in the panes of my bedroom window. It is a very different kind of existence at Tansonville now with Mme. de Saint-Loup, and a different kind of pleasure that I now derive from taking walks only in the evenings, from visiting by moonlight the roads on which I used to play, as a child, in the sunshine; while the bedroom, in which I shall presently fall asleep instead of dressing for dinner, from afar off I can see it, as we return from our walk, with its lamp shining through the window, a solitary beacon in the night. These shifting and confused gusts of memory never lasted for more than a few seconds; it often happened that, in my spell of uncertainty as to where I was, I did not distinguish the successive theories of which that uncertainty was composed any more than, when we watch a horse running, we isolate the successive positions of its body as they appear upon a bioscope.");
+var div_array = [];
+var n = text_array.length;
+for (let i = 0; i < n; i++){
+  div_array = div_array.concat(document.createElement('div'));
+}
+
+
+var offset_x = 50;
+var x_dir = 1;
+var offset_y = 50;
+$(document).ready(function() {
+  for(let d of div_array){
+    $(d).css("left", offset_x);
+    $(d).css("top", offset_y);
+    $(d).css("position", "absolute");
+    $("body").append(d);
+    if(offset_x >= $(window).width() - (100+$(d).width())){
+      x_dir = -1;
+    } else if(offset_x <= 50){
+      x_dir = 1;
+    }
+    offset_x += 50*x_dir;
+    offset_y += 50;
+  }
+
+  for(let i = 0; i < div_array.length; i++){
+    $(div_array[i]).text(text_array[i]);
+  }
+
+  $("div").draggable({
+    stack: "div",
+    grid: [50, 50],
+    // start: function(event, ui){
+		// 	$(this).css("transition", "0s");
+		// },
+		// stop: function(event, ui){
+		// 	$(this).css("transition", "0.25s");
+		// }
+  });
+
+  $("div").click(function(){
+    $(this).css('z-index', 2);
+    $(this).siblings('div').css('z-index', 1);
+
+
+  });
+
+  var intervalID;
+  $("div").hover(function(){
+    var old_top = parseInt($(this).css('top'), 10);
+    // var old_left = parseInt($(this).css('left'), 10);
+    $(this).css('top', old_top-(Math.floor(Math.random()*10)+5));
+    // $(this).css('left', old_left+Math.floor(Math.random()*2)-1);
+
+    var ref = this;
+    intervalID = setInterval(function(){
+      new_string = changeString($(ref).text());
+      $(ref).text(new_string);
+    }, (Math.floor(Math.random()*5000 + 5000)));
+
+  }, function(){
+    var old_top = parseInt($(this).css('top'), 10);
+    // var old_left = parseInt($(this).css('left'), 10);
+    $(this).css('top', old_top+(Math.floor(Math.random()*10)+5));
+    // $(this).css('left', old_left-Math.floor(Math.random()*2)-1);
+    // clearInterval(intervalID);
+  });
+
+
+  $(window).on('resize', function(){
+    offset_x = 50;
+    x_dir = 1;
+    offset_y = 50;
+    console.log("resize");
+    for(let d of div_array){
+      $(d).css("left", offset_x);
+      $(d).css("top", offset_y);
+      if(offset_x > $(window).width() - (150+$(d).width())){
+        x_dir = -1;
+      } else if(offset_x <= 50){
+        x_dir = 1;
+      }
+      offset_x += 50*x_dir;
+      offset_y += 50;
+    }
+  });
+
+});
+
+
+
+function changeString(string){
+  var words = RiTa.tokenize(string);
+  var rand = Math.floor(Math.random()*words.length);
+  var tag = RiTa.getPosTags(words[rand]);
+  var new_word = RiTa.randomWord(tag[0]);
+  // console.log(new_word);
+  words[rand] = new_word;
+  return RiTa.untokenize(words);
+}
