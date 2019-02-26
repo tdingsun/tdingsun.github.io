19a887eaa29485535a0fa9bca50fcb49293d5b35
tdingsun <tiger@tigers-mbp.risd.edu>
2019-02-26 03:27:13 -0500

    Added variation in layout

--- dataset/script.js
+++ dataset/script.js
@@ -1,4 +1,7 @@
+
+
 initGrid();
+
 
 
 
@@ -6,14 +9,28 @@ initGrid();
 function initGrid() {
   var grid = new Muuri('.grid', {
     dragEnabled: true,
-    layoutOnInit: false,
+    layoutOnInit: true,
+    layout: {
+      fillGaps: true
+    }
   }).on('move', function () {
-    saveLayout(grid);
+    //saveLayout(grid);
   });
 
-  $("#filter-places").click(function() {
-    var str = '.place'
-    filterItems(grid, str);
+  $(".item").each(function(){
+  var rand = Math.floor(Math.random() * 200) + 400;
+  $(this).css("width", rand);
+  console.log("heyall");
+})
+
+
+  $(".filter-btn").click(function() {
+    var str = $(this).attr('id').split('-')[1];
+    str = ".".concat(str);
+
+    if(str != ".all"){
+      filterItems(grid, str);
+    }
   });
 
   $("#filter-all").click(function() {
@@ -32,38 +49,38 @@ function initGrid() {
 
 }
 
-function serializeLayout(grid) {
-  var itemIds = grid.getItems().map(function (item) {
-    return item.getElement().getAttribute('data-id');
-  });
-  return JSON.stringify(itemIds);
-}
+// function serializeLayout(grid) {
+//   var itemIds = grid.getItems().map(function (item) {
+//     return item.getElement().getAttribute('data-id');
+//   });
+//   return JSON.stringify(itemIds);
+// }
 
-function saveLayout(grid) {
-  var layout = serializeLayout(grid);
-  window.localStorage.setItem('layout', layout);
-}
+// function saveLayout(grid) {
+//   var layout = serializeLayout(grid);
+//   window.localStorage.setItem('layout', layout);
+// }
 
-function loadLayout(grid, serializedLayout) {
-  var layout = JSON.parse(serializedLayout);
-  var currentItems = grid.getItems();
-  var currentItemIds = currentItems.map(function (item) {
-    return item.getElement().getAttribute('data-id')
-  });
-  var newItems = [];
-  var itemId;
-  var itemIndex;
+// function loadLayout(grid, serializedLayout) {
+//   var layout = JSON.parse(serializedLayout);
+//   var currentItems = grid.getItems();
+//   var currentItemIds = currentItems.map(function (item) {
+//     return item.getElement().getAttribute('data-id')
+//   });
+//   var newItems = [];
+//   var itemId;
+//   var itemIndex;
 
-  for (var i = 0; i < layout.length; i++) {
-    itemId = layout[i];
-    itemIndex = currentItemIds.indexOf(itemId);
-    if (itemIndex > -1) {
-      newItems.push(currentItems[itemIndex])
-    }
-  }
+//   for (var i = 0; i < layout.length; i++) {
+//     itemId = layout[i];
+//     itemIndex = currentItemIds.indexOf(itemId);
+//     if (itemIndex > -1) {
+//       newItems.push(currentItems[itemIndex])
+//     }
+//   }
 
-  grid.sort(newItems, {layout: 'instant'});
-}
+//   grid.sort(newItems, {layout: 'instant'});
+// }
 
 function filterItems(grid, str) {
   grid.filter(str);
