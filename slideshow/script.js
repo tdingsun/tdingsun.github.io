456b428f82277ec214eecbb1e88795f04f9b04d5
tdingsun <tiger@tigers-mbp.risd.edu>
2019-05-08 19:23:05 -0400

    Home page

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,16 +1,17 @@
 var width;
 var height;
 var divWidth;
+const numImages = 143;
 const margin = 20;
 const fontsize = parseInt($("body").css("font-size"));
-var regex = /[,."“?()]+/;
+var regex = /([,."“()]+)/;
 
-var str = "I used to have this habit of taping small images to the walls of my bedroom. photos, postcards, and drawings from here and there. A couple of weeks ago, I was staring at the constellation of images on my walls, somewhat listlessly, when I suddenly got so tired of them. Suddenly every image seemed utterly replaceable with any other image. Entirely uninteresting, it seemed like any source of uniqueness or value merely came from an arbitrary reordering of ink on paper. The image is a lifeless being. I asked myself: do I have any real attachment to any piece of graphic work? Have I ever? If not, what even is the point of claiming to be an image-maker? The bare walls and the blank pieces of paper seemed to offer so much more prospect and possibility, and any commitment to a particular decision seemed so arbitrary and limiting. I never want to see the same image twice ever again. This is, of course, the most tongue-in-cheek of statements, but there is some truth to the sentiment.";
-var sentences = str.split(regex);
+var str = "I used to have this habit of taping small images to the walls of my bedroom.\\ photos,\\ postcards,\\ and drawings from here and there.\\ A couple of weeks ago,\\ I was staring at the constellation of images on my walls,\\ somewhat listlessly,\\ when I suddenly got so tired of them.\\ Suddenly every image seemed utterly replaceable with any other image.\\ Entirely uninteresting,\\ it seemed like any source of uniqueness or value merely came from an arbitrary reordering of ink on paper.\\ The image is a lifeless being.\\ I asked myself:\\ do I have any real attachment to any piece of graphic work?.\\ Have I ever?.\\ If not,\\ what even is the point of claiming to be an image-maker?.\\ The bare walls and the blank pieces of paper seemed to offer so much more prospect and possibility,\\ and any commitment to a particular decision seemed so arbitrary and limiting.\\ I never want to see the same image twice ever again.\\ This is, of course,\\ the most tongue-in-cheek of statements,\\ but there is still some truth to the sentiment.";
+var sentences = str.split("\\");
 
 var synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 var notes = Tone.Frequency("G2").harmonize([0, 4, 7, 12, 16, 19, 24, 28, 31, 36, 40]);
-
+var randNote;
 StartAudioContext(Tone.context, 'div').then(function(){
   //started
   console.log("clicked");
@@ -18,7 +19,7 @@ StartAudioContext(Tone.context, 'div').then(function(){
 });
 
 //have to click to start audio context
-$('div').click(function(){
+$(document).click(function(){
   Tone.start();
   console.log("clicked");
 });
@@ -33,28 +34,36 @@ $(document).ready(function(event){
   // $('<img src="' + path + '">').load(function(){
   //   $("#picture").append($(this));
   // })
-  setTimeout(loadImage, 1000, 0);
+  setTimeout(loadImage, 1000, 1);
   setTimeout(loadText, 1000, 0);
 
 });
 
 function loadText(index){
+  randNote = Math.floor(Math.random()*notes.length);
+  synth.triggerAttackRelease(notes[randNote], "1n");
   $("#subtitle").text(sentences[index]);
   var time = sentences[index].length * 50;
   if(time < 1000){
     time = 1000;
   }
-  setTimeout(loadText, time, index + 1);
+  if(index < sentences.length){
+    setTimeout(loadText, time, index + 1);
+  }
 
 }
 
 function loadImage(index){
+  randNote = Math.floor(Math.random()*notes.length);
+  synth.triggerAttackRelease(notes[randNote], "1n");
+
   var path = "images/" + index + ".jpg";
   $('#picture').css({
     "background-image": 'url("' + path + '")'
   });
-
-  setTimeout(loadImage, 500, index + 1);
+  if(index < numImages){
+    setTimeout(loadImage, 500, index + 1);
+  }
 }
 
 
