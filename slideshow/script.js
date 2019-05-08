1f20f17ece53f4bd669c6631c000d2cbe83fc5d9
tdingsun <tiger@tigers-mbp.risd.edu>
2019-05-08 16:34:06 -0400

    Slideshow changes

--- slideshow/script.js
+++ slideshow/script.js
@@ -3,12 +3,10 @@ var height;
 var divWidth;
 const margin = 20;
 const fontsize = parseInt($("body").css("font-size"));
+var regex = /[,."“?()]+/;
 
-var str = "Homage to dictee // Peal, that word peal. A bell, the clearest tone, a blue so light it is almost transparent, but also thunder and laughter. Peal, I read on the last page of a book, peal, I try to imitate. A plastic cross and through that, a silhouette of bird black and soaring. Before that, chain link hole tree thickening through. Deeper as I crane my neck, lighter towards the horizon flocks bell peal, a full ringing, perfect oscillation, a child’s arms outstretched, raise me towards the sky, towards the sky the skies up there carry me forward falling asleep slowly rolling on your back. Self-admonishment paints you well, Through self-denial only enlightenment. Come. Come here, please. Turn back and so will I. Will this to propel me forward. Will this to be enough, the question remains. You, like a brother, reciprocation with something you are more likely like familial love, something like the word I can replace it with when it occurs to me. Starts to subside only partially through jealousy and love. The pangs of thinking around you start as soon as we part ways for the day. Quiet tongue held scratching heavily pen on paper. Object objective to love lover loving in a loving way. /// A loving way, loving lover love to object.  Paper, pen, heavy scratching held quiet tongue. The day, we part ways, soon around you to start thinking the pains. Of love and something else, starting to subside only partially when it occurs. Desire replaced with something like familial love, likely something you reciprocate, brother. You, a brother, remains the question. Enough to propel me forward, I will so and to turn back. Please, come here. Come here, please. Enlightenment only through self-denial Painting you well with self-admonishment. Back rolling slowly asleep, falling, lift me up the skies the sky lift me up towards the sky, outstretched like a child’s arms, perfections and a full ringing, peals the bell flocks horizon towards light, crane my neck and I see deeper blue. Through thickening trees hole link chain fence, before that soaring and black bird shadow and through that, a cross and me looking out. Try to imitate a bell, peals, the last page of a book, peal, of laughter and of thunder. A blue so light, almost transparent, the clearest tone, a bell peals, peal, that word, beautiful that word peal. ";
-var words_arr = str.split(" ");
-var word_index = 0;
-var position_index = 0;
-var word_queue = [];
+var str = "I used to have this habit of taping small images to the walls of my bedroom. photos, postcards, and drawings from here and there. A couple of weeks ago, I was staring at the constellation of images on my walls, somewhat listlessly, when I suddenly got so tired of them. Suddenly every image seemed utterly replaceable with any other image. Entirely uninteresting, it seemed like any source of uniqueness or value merely came from an arbitrary reordering of ink on paper. The image is a lifeless being. I asked myself: do I have any real attachment to any piece of graphic work? Have I ever? If not, what even is the point of claiming to be an image-maker? The bare walls and the blank pieces of paper seemed to offer so much more prospect and possibility, and any commitment to a particular decision seemed so arbitrary and limiting. I never want to see the same image twice ever again. This is, of course, the most tongue-in-cheek of statements, but there is some truth to the sentiment.";
+var sentences = str.split(regex);
 
 var synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 var notes = Tone.Frequency("G2").harmonize([0, 4, 7, 12, 16, 19, 24, 28, 31, 36, 40]);
@@ -31,20 +29,34 @@ $(document).ready(function(event){
   // } else {
   //   $(".container").html("Welcome to the world of disconnection");
   // }
-  var folder = "images/";
 
-  $.ajax({
-      url : folder,
-      success: function (data) {
-          $(data).find("a").attr("href", function (i, val) {
-              if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                  $("body").append( "<img src='"+ folder + val +"'>" );
-              } 
-          });
-      }
-  });
+  // $('<img src="' + path + '">').load(function(){
+  //   $("#picture").append($(this));
+  // })
+  setTimeout(loadImage, 1000, 0);
+  setTimeout(loadText, 1000, 0);
 
 });
+
+function loadText(index){
+  $("#subtitle").text(sentences[index]);
+  var time = sentences[index].length * 50;
+  if(time < 1000){
+    time = 1000;
+  }
+  setTimeout(loadText, time, index + 1);
+
+}
+
+function loadImage(index){
+  var path = "images/" + index + ".jpg";
+  $('#picture').css({
+    "background-image": 'url("' + path + '")'
+  });
+
+  setTimeout(loadImage, 500, index + 1);
+}
+
 
 $(window).resize(function(){
   resize();
@@ -59,3 +71,4 @@ function resize(){
   height = $(window).height();
   divWidth = width/3 - margin;
 }
+
