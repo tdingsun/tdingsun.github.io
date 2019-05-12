a3be5596999a8780ee64744ebca9e2e74e440298
tdingsun <tiger@Tigers-MBP.fios-router.home>
2019-05-12 10:19:55 -0400

    Next sag

--- glossary/script.js
+++ glossary/script.js
@@ -1,63 +1,49 @@
 var width;
 var height;
-const numImages = 163;
 
 
 var svgArray = [];
+var currIndex = 0;
 
 
 
 
 $(document).ready(function(event){
 
-  loadSVGs();
+  loadAllSVGs(); //load all SVGs
+  $("#picture").append(svgArray[currIndex]); //start with the first one
 
-
-  jQuery.get("images/2.svg", function(data){
-    var $svg = jQuery(data).find('svg');
-    $svg.attr("preserveAspectRatio", "none");
-    $svg.find("line").each(function(){
-      $(this).attr("preserveAspectRatio", "none");
-    });
-    $svg.find("rect").each(function(){
-      $(this).attr("preserveAspectRatio", "none");
-    });
-    $svg.find("path").each(function(){
-      $(this).attr("preserveAspectRatio", "none");
-    });
-    $("#picture").append($svg);
-  });
-
-  // $(window).scroll(function(){
-  //   console.log("scroll");
-  //   randomize();
-  // });
-
+  //event listener for scroll: have to do it this way because of chrome
   window.addEventListener("wheel", randomize, {passive: false});
 
-
-
-  $("#picture").click(function(){
-    randomize();
+  $("#picture").click(function(){ //a click will randomize it as well
+    nextSVG();
   });
-
-  document.onscroll  = randomize;
 });
-
 
 $(window).resize(function(){
   resize();
-  randomize();
 });
+
+function nextSVG(){
+  if(currIndex == svgArray.length - 1){
+    currIndex = 0;
+  } else {
+    currIndex++;
+  }
+  $("#picture").empty();
+  $("#picture").append(svgArray[currIndex]);
+}
 
 function randomize(){
   var randIndex = Math.floor(Math.random() * svgArray.length);
+  currIndex = randIndex;
   $("#picture").empty();
   $("#picture").append(svgArray[randIndex]);
   console.log("rando");
 }
 
-function loadSVGs() {
+function loadAllSVGs() {
   for(let i = 2; i < 76; i++){
     jQuery.get("images/" + i + ".svg", function(data){
       var $svg = jQuery(data).find('svg');
@@ -81,15 +67,5 @@ function loadSVGs() {
 function resize(){
   width = $(window).width();
   height = $(window).height();
-}
-
-function shuffle(array){
-  for(let i = 0; i < array.length; i++){
-    var randIndex = Math.floor(Math.random()*array.length);
-    var tmp = array[i];
-    array[i] = array[randIndex];
-    array[randIndex] = tmp;
-  }
-  return array;
 }
 
