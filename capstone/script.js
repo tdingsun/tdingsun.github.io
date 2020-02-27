83b72a036b04d9ea22a8a8a8e236b71935ad4dcc
Tiger <tiger.dingsun@gmail.com>
2020-02-27 15:20:43 -0500

    nav highlighting

--- capstone/script.js
+++ capstone/script.js
@@ -1,28 +1,14 @@
-// function transformScroll(event) {
-//     if (!event.deltaY) {
-//       return;
-//     }
-//     console.log("scrolling");
-//     event.currentTarget.scrollLeft += event.deltaY;
-//     event.preventDefault();
-//   }
-  
-//   var element = document.scrollingElement || document.documentElement;
-//   element.addEventListener('wheel', transformScroll);
 
 // var colors = ["#fcf", "#fcc", "#fc9", "#fc6", "#fc3", "#fc0"];
-// var cc = 0
 var l = 0;
 var new_l = 0;
 
 $(document).ready(function(){
     $(window).keydown(function(event){
         event.preventDefault();
-        var currPos = $(window).scrollLeft()
+        var currPos = $(window).scrollLeft();
         remainder = Math.floor(currPos) % Math.floor(window.innerWidth);
         if(event.key === "ArrowLeft"){
-            // $("body").css("background-color", colors[cc]);
-            // cc = (cc - 1) % colors.length
             if (remainder <= 1){
                 $(window).scrollLeft((currPos - remainder) - window.innerWidth);
             } else {
@@ -30,8 +16,6 @@ $(document).ready(function(){
             }
         }
         if(event.key === "ArrowRight"){
-            // $("body").css("background-color", colors[cc]);
-            // cc = (cc + 1) % colors.length
             $(window).scrollLeft((currPos - remainder) + window.innerWidth);
         }
     })
@@ -46,14 +30,12 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         for(let i = num_chapters; i > 0; i--){
-            console.log("hi");
             if (locations[i-1] <= $(window).scrollLeft()){
                 new_l = i;
                 break;
             }
         }
-        console.log(l)
-        console.log(new_l)
+
         if(l != new_l){
             l = new_l
             $(".navitem").css({
@@ -65,5 +47,17 @@ $(document).ready(function(){
                 'color': "pink"    
             });
         }
+    });
+
+    $('.navitem').click(function(e){
+        e.stopPropagation();
+        $(".navitem").css({
+            'background-color': "pink",
+            'color': "blue"           
+        });
+        $(this).css({
+            'background-color': "blue",
+            'color': "pink"    
+        });
     });
 });
