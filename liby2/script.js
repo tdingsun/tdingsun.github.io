a60891237bc2df6fbefaeed6cf37dbc77f5e62d7
students <students@GD-Loaner-B.fios-router.home>
2020-04-23 14:12:21 -0400

    liby2 init

---
+++ liby2/script.js
@@ -0,0 +1,138 @@
+var width;
+var height;
+var title = "Can’t Run Trip Over Your Fucking Swag";
+var author = "Liby Hays"
+var speed = 1000;
+
+var part1 = ["Historically, certain boys have taken it upon themselves, on occasions more or less opportune—sometimes at public functions but more often on dun-colored carpets in finished basements at the oversugared tail ends of birthday parties, to do a dance known as “the worm.”", "The dance is performed with belly to the floor. The challenge is for a pelvic undulation to seamlessly reintegrate between opposite ends of the body, so as legs lift off the ground arms brace against the fall and raise the body back up to initiate a new waveform. This is the premise, but there are many known variations, including the variation where the boy stays fixedly propped up on his arms and pumps his legs back, going nowhere, or where the boy rocks back and forth on his torso with splayed knees and crossed ankles, lifting himself in breaching jolts like a froggy, nascent spazz.", "The dance has been the subject of much speculation. No firsthand account of worming is known to exist. It’s debated whether the wormer’s life has yet taken on the continuity where he could look back and say, “I was...”", "For the wormer, they posit, affects come and go in a superfluous stream, of which the dance is the purest expression."
+];
+
+var part2 = ["“Hey,” said Beyond Birthday. But what she really meant was, “Hey, you.” It was eighth period art class—the period only existing because buses from afterschool activities had been defunded, and the rest of the student body had to be babysat while the orchestra was in session—and Beyond Birthday was squatting on the linoleum floor beneath the trifold board to which Airheads, Cheetos, and other sundries were affixed.", "“Hey, do you think you can help me out? Can you take these...” handing you a package of Lemonheads, “and put them under the door?”", "Beyond was the monitor for the ‘Trudeau Store,’ where you could buy candy with Xerox paper money of the art teacher Ms.Trudeau’s face, which she gave out for cleaning your desk of construction paper scraps and glue smears in a timely manner. Beyond was corrupt, but despite fear of retribution you did her bidding and put the Lemonheads on the other side of the door while she pretended to look the other way.", "A few weeks later, in the same hallway, you, Beyond Birthday, and friend Erica were asked to clear an exhibit of blind contour drawings off the wall. What happened next will always confound you—perhaps it was a retread of when a substitute teacher caught Beyond passing a note and she swallowed it without question. Whatever the case, now forever tucked in some eidetic crevice of your hindbrain is the line of Beyond’s grin as a continuation of the scribbled linework, paper buckling against her gleaming incisors.", "***", "Beyond wore cardigans, grey jeans and 2-inch patent leather heels. Her arms were often crossed. Sometimes she tattooed herself during study hall with pen ink and a pile of pushpins.", "And when Beyond would whisper, every syllable was worn so thin, it could barely veil (or veil only in spirit) the glimmering kernel of fascism that lay within and beyond it.", "***", "She was going back to get a knife. You hadn’t seen the fight— not exactly—but information had reverberated, snaking along grassy whorls from park center to the outer bounds.", "Up until this day, the park never felt like a world unto itself. It was an open and vulnerable park bounded by train tracks on one side, so commuting businesspeople were interspersed with the preteens and soccer players at certain times of day. It was a place you might go to smoke table sugar rolled in a receipt, or ask a stranger to break up with your girlfriend for you.", "But this day, for whatever reason, there was mob action—droves of seventh grade boys storming the green with switchblades pulled or threatened, older siblings and cousins cited as shadowy backup defense, in what could only be described as a brouhaha. Things move too quickly for you to process, but you’re made to understand them through a real-time rumor circuit.", "Beyond’s scrape happens later in the afternoon. A girl from another middle school had instigated over an online comment, and while you don’t witness the action, you feel its torque—hubbub spiraling out from the site of the encounter. And from afar you see BB skulking off to get the knife, fuming and without posse, straight-ironed hair slightly mussed.", "She doesn’t come back to the park, but you spot her later that night with a black eye, waiting under a movie theatre awning for her mom to pick her up.", "***", "The fight gains BB plenty of notoriety in school the next week. But she is still best known for her antics in the other uptown park, giving blowjobs in the janitor’s toolshed, and for downing twelve water bottles of vodka during a single lunch break.", "Not long after this point, her family moves away and she’s sent to reform school."]
+
+var part3 = [
+  "Beyond Birthday, whose whole life was one long pantomime of a throat being slit..."
+]
+
+var part4 = [
+  "The negative obverse of the worm dance is a wrist dance. It’s not called a wrist dance, it has no name, but it can be referred to as a wrist dance for the sake of convenience.", 
+  "The wrist dance involves two parties. One party is a girl, dropped off at sleepaway camp earlier in the day and attending the welcoming bash and the second party is her counselor, a high school girl probably four years older but obviously still seeing the camper as a little kid, and enforcing this belief through the medium of dance. She grabs the new camper’s wrists and pulls them back and forth like joysticks, a dance for partnering with a toddler or a puppet but not a mortified stranger, as if one could be forcibly yanked into having fun.",
+  "And if the worm is an expression of lightness, of wriggly, embodied mirth, then the wrist dance is like wax cast within a flayed skin, both the body’s leaden self-containment and peeled inversion. Or as if one could reach out and touch the disassociated self that hovers drone-like above the premises in moments of hallucinogenic split consciousness, and then conceive a genetically redundant child with the disassociated self—the feeling of looking that child square in the face and admonishing it for things beyond its control, was the girl’s feeling as the camp instructor gripped her wrists and forced her to dance.",
+  "Vengeance could not be enacted directly. It could only be enacted through another false gesture, victimizing not the perpetrator but an enthusiastic nerd boy a year her junior.",
+  "It was the last night of camp. Everyone was shepherded in to watch wooden letters spelling the name of the camp burn on the opposite river bank. The idea behind the flames (or so she decided) was to bake nostalgia into memories of the very recent past.",
+  "The girl wrinkled her chin and began mewling. The boy came up to her, tear-streams aquiver, and the two wailed in unison and embraced. Then the girl pulled back and straightened her face with a guffaw. “Haha! I’m not really crying!”",
+  "What are tears worth here, she had thought. Certainly they were worth nothing the night after the dance when I ran off into the woods in my socks. Maybe they were worth something when according to legend a camper last year cried after burgers and fries hailed down from a helicopter as the opening salvo of Color Wars, because he or she was so disappointed that the battle hadn’t been announced in a more grandiose way... So it’s not like anyone at camp cries for a good reason.",
+  "But the boy didn’t express disappointment. He was instead seized for an instant by blind rage. It’s the kind of emotion you witness rarely, especially if you’re not the type to provoke. The next time she would be receive a look of this order it would be many years later. (And the man was a police.)"
+]
+
+var part5 = [
+  "(Beyond Birthday's Perspective)",
+  "Where are these fucking people from? Planet eat drink and clothe myself? What gives them the right to be so boring. In CVS I see ten different people and they’re all boring as fuck and I wish they would get out of my beautiful mental landscape.",
+  "The guy who drove me here is probably kind of boring too, but he’s okay and I luuuuuuuuurve him or whatever. He’s under my spell at the moment. Yeah, the mystification only goes one way, but my life is pointy like that.",
+  "Sometimes I feel like my life is that game, Black Magic, where you pretend you can psychically transmit a the name of an object to your partner. Your partner closes their eyes and you point out an object for everyone else to see. Then you start listing things in the room: “that chair...” “the oak paneling...” “that boy’s shirt...” and the partner goes no, no, yes! And they know what object you were secretly thinking of because it’s the first object that you named that was black. Not sure who in this scenario represents me or my boyfriend or all men or all people... actually, I think I’m the black shirt.",
+  "My brother is a coder now, so he only eats flat foods you can slide under a locked door. I only eat shitty foods you can get from CVS. I like Oreos because they look like manhole covers, which are all that stands between us and literal shit. Otherwise just black coffee for me, thanks.",
+  "For a hot second I was terrified that someone was trying to poison me. I wouldn’t eat all day, and then I would go to CVS at 9 at night and shoplift a bunch of random foods. I thought because I hadn’t paid no one would be able to track them and transmit the poison to me. I bet I would’ve been caught if I wasn’t so young and cute at the time.",
+  "My boyfriend is complaining about not being able to decide whether to leave his job or not, because he keeps getting promoted and it’s not a career he really wants, installing sprinkler systems, and it’s only going to take more and more time away from his music. I tell him it’s sort of like the point where, in guided meditation, you’ve been imagining this ball of white light traveling from the crown of your head down your face and neck and torso, but once It reaches your crotch you’re faced with a bunch of split decisions. I don’t know if he appreciates my commentary. He might not be in the mood.",
+  "I’m not a perfectionist, but I share certain qualities with perfectionists, like the failure to delegate tasks. But I’m trying to improve by giving my boyfriend more tasks to do. So by the brick wall outside CVS I ask him to take a photo of me with a disposable camera—the kind with yellow paper covering, the little clickety wheel you roll back, and film no one develops anymore. He’s asks why I’m using this camera when our phones work perfectly well.",
+  "I’ll admit the idea is somewhere along the lines of perfectionism. Because I want redeye in the photos, I tell him. I want everyone to see my blood."
+]
+
+var parts = [part1, part2, part3, part4, part5];
+
+var keywords = {"dance": ["dance"], 
+              "boy": ["boy", "boys", "boyfriend"], 
+              "school": ["school", "afterschool"], 
+              "girl": ["girl", "girls", "girlfriend"], 
+              "life": ["life"],
+              "camp": ["camp", "camper", "campers"],
+              "wrist": ["wrist", "wrists"],
+              "worm": ["worm", "wormer", "wormers", "worming"],
+              "the": ["the"]
+            }
+var word_index = 0;
+
+var synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
+var notes = Tone.Frequency("G4").harmonize([0, 4, 7, 11, 14, 17, 21, 24, 28, 31, 28, 24, 21, 17, 14, 11, 7, 4]);
+
+StartAudioContext(Tone.context, 'div').then(function(){
+  //started
+  console.log("clicked");
+
+});
+
+$(document).ready(function(event){
+  width = $(window).innerWidth();
+  height = $(window).innerHeight();
+  displayTitle(title, author);
+  setTimeout(setupText, 1500);
+
+  setInterval(rotateVertical, 1000);
+  setInterval(rotateHorizontal, 2000);
+});
+
+$("#container").on("click", ".btn", function(event){
+  $(this).toggleClass("selected");
+  let key = $(this).text();
+  keywords[key].forEach(function(word, index){
+    $(`.${word}`).toggleClass("highlight");
+  });
+})
+
+
+function displayTitle(title, author){
+  $("#title").html(title + "<br>by " + author);
+}
+
+function setupText(){
+  parts.forEach(function(part, index){
+    let newCol = $("<div class='col'></div>");
+    let newPart = $("<div class='part'></div>");
+    newCol.append($(`<h1>${index + 1}</h1>`));
+    newCol.append(newPart);
+
+    part.forEach(function(paragraph, index){
+
+      let newP = $("<p></p>");
+      words = paragraph.split(" ");
+
+      words.forEach(function(word, index) {
+        let newSpan = $(`<span class='${word.replace(/[!"#$%&'’”“()*+,-./:;<=>?@[\]^_`{|}~]/g, '')}'></span>`);
+        newSpan.text(word);
+        newP.append(newSpan);
+      });
+      newPart.append(newP);
+    });
+    if(index == 2){
+      newPart.append($("<div id='panel'><div>"));
+    }
+    $("#container").append(newCol);
+  });
+
+  fillPanel();
+
+  $("#title").css({
+    "font-size": "3rem"
+  });
+}
+
+function fillPanel() {
+  for (const [key, value] of Object.entries(keywords)){
+    $("#panel").append($(`<div id='${key}-btn' class="btn">${key}</div>`));
+  } 
+}
+
+var currRotateV = 0;
+var currRotateH = 0;
+
+function rotateVertical() {
+  currRotateV += 15;
+  $("#vertical").css({
+      "transform": `rotate(${currRotateV}deg)`
+  })
+}
+
+function rotateHorizontal() {
+  currRotateH += 7.5;
+  $("#horizontal").css({
+      "transform": `rotate(${currRotateH}deg)`
+  })
+}
