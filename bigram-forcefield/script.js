e38d3bfb548629a18ff0a2afcbc21c120246cc1c
tdingsun <tiger.dingsun@gmail.com>
2023-04-14 18:35:19 -0400

    cleanup

--- bigram-forcefield/script.js
+++ bigram-forcefield/script.js
@@ -5,15 +5,14 @@ const windowHeight = window.innerHeight;
 
 const color = 'dodgerblue';
 const border = 100;
-const scale = 1000;
 const spacing = 49; //has to be odd
 const barrier = 5000;
 const q_limit_top = 60000;
 const q_limit_bottom = -1 * q_limit_top;
-let q_delta = 100
-
+var charges = [];
 const radius = windowWidth * 0.01;
 const density = 100;
+
 const fieldCanvas = document.getElementById("fieldCanvas");
 const ctx = fieldCanvas.getContext("2d");
 fieldCanvas.width = windowWidth
@@ -22,19 +21,15 @@ ctx.font = "12px sans-serif";
 ctx.fillStyle = color;
 ctx.strokeStyle = color;
 
+var q_delta = 50
 class Charge {
-    x = 0;
-    y = 0;
     q;
     body;
-
     constructor(init_q, body) {
-        this.q = init_q * scale
+        this.q = init_q
         this.body = body
     }
     step() {
-        this.x = this.body.position.x
-        this.y = this.body.position.y
         this.q += q_delta
         if (this.q > q_limit_top || this.q < q_limit_bottom) {
             q_delta *= -1
@@ -166,15 +161,15 @@ const words = shuffle([
     ['POP', 'TWO'],
     ['ANTI', 'FRAGILE'],
     ['CORN', 'FED'],
+    ['BINKY', 'BONG'],
+    ['KICK', 'ROCKS'],
 ])
 
-//objects
+//html objects
 const display = document.getElementById("display");
 const list = document.getElementById("list");
 
 //setup words
-let first_word;
-let second_word;
 display.innerHTML = words[0][0] + '<br>' + words[0][1]
 list.innerHTML += '<div>' + words[0][0] + ' ' + words[0][1] + '</div>';
 let wordsIndex = 0;
@@ -215,95 +210,98 @@ var runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
-
-
-let charges = [];
-
 //on resize
 window.onresize = function () { location.reload(); }
 
-//setup 
+////////////////////////////
+
+//setup
 setupWalls();
-setupSpheres();
+setupCharges();
 
 //main
 switchWords();
 window.requestAnimationFrame(step)
 
+////////////////////////////
 
+//helper functions
+var word1_before, word2_before;
+var word1_after, word2_after;
+var maxWordLength;
+var limit;
+var result_1;
+var result_2;
 function switchWords() {
-    let word1_before;
-    let word2_before;
     if (wordsIndex === 0) {
         word1_before = words[words.length - 1][0];
         word2_before = words[words.length - 1][1];
     } else {
-        word1_before = words[wordsIndex - 1][0]
-        word2_before = words[wordsIndex - 1][1]
+        word1_before = words[wordsIndex - 1][0];
+        word2_before = words[wordsIndex - 1][1];
     }
-    let word1_after = words[wordsIndex][0]
-    let word2_after = words[wordsIndex][1]
-    let maxWordlength = max(word1_before.length, word1_after.length, word2_before.length, word2_after.length);
+    word1_after = words[wordsIndex][0];
+    word2_after = words[wordsIndex][1];
+    maxWordLength = max(word1_before.length, word1_after.length, word2_before.length, word2_after.length);
+    limit = maxWordLength - 1;
+    setTimeout(switchLetters, wordTiming, 0)
+}
 
-    setTimeout(switchLetter, wordTiming, 0)
-
-    function switchLetter(idx) {
-        let result_1 = word1_after.substring(0, idx) + word1_before.substring(idx, word1_before.length);
-        let result_2 = word2_after.substring(0, idx) + word2_before.substring(idx, word2_before.length);
-        display.innerHTML = result_1 + '<br>' + result_2
-        if (idx <= maxWordlength - 1) {
-            setTimeout(switchLetter, wordTiming, idx + 1)
-        } else {
-            list.innerHTML += '<div>' + words[wordsIndex][0] + ' ' + words[wordsIndex][1] + '</div>';
-            setTimeout(() => {
-                wordsIndex++;
-                if (wordsIndex >= words.length) {
-                    wordsIndex = 0;
-                    list.innerHTML = '';
-                }
-                switchWords();
-            }, 1000);
-        }
+function switchLetters(idx) {
+    result_1 = word1_after.substring(0, idx) + word1_before.substring(idx, word1_before.length);
+    result_2 = word2_after.substring(0, idx) + word2_before.substring(idx, word2_before.length);
+    display.innerHTML = result_1 + '<br>' + result_2
+    if (idx <= limit) { //switching letters
+        setTimeout(switchLetters, wordTiming, idx + 1)
+    } else { //end condition
+        list.innerHTML += '<div>' + words[wordsIndex][0] + ' ' + words[wordsIndex][1] + '</div>';
+        setTimeout(() => {
+            wordsIndex++;
+            if (wordsIndex >= words.length) {
+                wordsIndex = 0;
+                list.innerHTML = '';
+            }
+            switchWords();
+        }, 1000);
     }
 }
 
+var charge;
 function step() {
-    for (let charge of charges) {
+    for (charge of charges) {
         charge.step()
     }
     draw()
     window.requestAnimationFrame(step)
 }
 
-let limit_x = fieldCanvas.width - border;
-let limit_y = fieldCanvas.height - border;
-let inverse_spacing = 1/spacing;
+const limit_x = fieldCanvas.width - border;
+const limit_y = fieldCanvas.height - border;
+var i, j, vector, parity, magnitude_2, angle, new_x, new_y
 function draw() {
     clearCanvas()
-   
-
     ctx.beginPath();
-    for (let i = border; i < limit_x; i += spacing) {
-        for (let j = border; j < limit_y; j += spacing) {
-            let vector = getFieldVector(i, j);
-            let parity = ((i + j)) & 1;
-            let magnitude_squared = (vector.x * vector.x) + (vector.y * vector.y);
-            let angle = fastAtan2(vector.y, vector.x);
-            let newPoint_x = i + vector.x
-            let newPoint_y = j + vector.y
+    for (i = border; i < limit_x; i += spacing) {
+        for (j = border; j < limit_y; j += spacing) {
+            vector = getFieldVector(i, j);
+            parity = (i + j) & 1;
+            magnitude_2 = (vector.x * vector.x) + (vector.y * vector.y);
+            angle = fastAtan2(vector.y, vector.x);
+            new_x = i + vector.x;
+            new_y = j + vector.y;
             ctx.save();
-            ctx.translate(newPoint_x, newPoint_y);
+            ctx.translate(new_x, new_y);
             ctx.rotate(angle);
-            if (magnitude_squared < 100) { //word
-                ctx.fillText(words[wordsIndex][parity], -5, 5)
+            if (magnitude_2 < 100) { //word
+                ctx.fillText(words[wordsIndex][parity], -5, 5);
                 ctx.restore();
             } else {
-                    ctx.moveTo(-4, -4);
-                    ctx.lineTo(-4, 4);
-                if (magnitude_squared < 10000) { // line and plus
+                ctx.moveTo(-4, -4);
+                ctx.lineTo(-4, 4);
+                if (magnitude_2 < 10000) { // line and plus
                     ctx.restore();
                     ctx.moveTo(i, j);
-                    ctx.lineTo(newPoint_x, newPoint_y)
+                    ctx.lineTo(new_x, new_y)
                 } else { // plus only
                     ctx.moveTo(0, 0);
                     ctx.lineTo(-8, 0);
@@ -315,47 +313,40 @@ function draw() {
     ctx.stroke();
 }
 
+var vector_acc, dx, dy, distance_2, m
 function getFieldVector(x, y) {
-    let vector = { x: 0, y: 0 }
+    vector_acc = { x: 0, y: 0 }
     for (let charge of charges) {
-        let delta_x = charge.x - x
-        let delta_y = charge.y - y
-        let distance_squared = delta_x * delta_x + delta_y * delta_y
-        if (distance_squared > barrier) {
-            let m = (charge.q / distance_squared)
-            vector.x += delta_x * m
-            vector.y += delta_y * m
-        }
+        dx = charge.body.position.x - x
+        dy = charge.body.position.y - y
+        distance_2 = dx * dx + dy * dy
+        m = (charge.q / distance_2)
+        vector_acc.x += dx * m
+        vector_acc.y += dy * m
     }
-    return vector
+    return vector_acc
 }
 
-function setupSpheres() {
-    //spheres
+function setupCharges() {
     charges = [
         new Charge(0, makeBody(windowWidth * 0.5, 200, 0, -10)),
-        new Charge(5, makeBody(200, windowHeight - 200, 10, -10)),
-        new Charge(-5, makeBody(windowWidth - 200, windowHeight - 200, -10, -10))
+        new Charge(3000, makeBody(200, windowHeight - 200, 10, -10)),
+        new Charge(-3000, makeBody(windowWidth - 200, windowHeight - 200, -10, -10))
     ]
 }
 
+var body;
 function makeBody(x, y, x_speed, y_speed) {
-    let body = Bodies.circle(
+    body = Bodies.circle(
         x, y, radius, {
-        density: density,
-        frictionAir: 0,
-        restitution: 1,
-        render: {
-            fillStyle: color,
-        },
-        plugin: {
-            attractors: [
-                MatterAttractors.Attractors.gravity
-            ]
+            density: density,
+            velocity: { x: x_speed, y: y_speed },
+            frictionAir: 0,
+            restitution: 1,
+            render: { fillStyle: color },
+            plugin: { attractors: [ MatterAttractors.Attractors.gravity ] }
         }
-    }
     );
-    Body.setVelocity(body, { x: x_speed, y: y_speed })
     World.add(world, body);
     return body
 }
@@ -389,14 +380,15 @@ function shuffle(a) {
     return a;
 }
 
-var fastAtan2 = (y, x) => {
-     let ax = abs(x);
-     let ay = abs(y);
-     let a = min(ax, ay) / max(ax,ay);
-     let s = a * a;
-     let r = ((-0.0464964749 * s + 0.15931422) * s - 0.327622764) * s * a + a;
-     if (ay > ax) r = PI2 - r;
-     if (x < 0) r = PI - r;
-     if (y < 0) r = -r;
-     return r;
+var ax, ay, a, s, r;
+function fastAtan2(y, x) {
+    ax = abs(x);
+    ay = abs(y);
+    a = min(ax, ay) / max(ax, ay);
+    s = a * a;
+    r = ((-0.0464964749 * s + 0.15931422) * s - 0.327622764) * s * a + a;
+    if (ay > ax) r = PI2 - r;
+    if (x < 0) r = PI - r;
+    if (y < 0) r = -r;
+    return r;
 }; 
